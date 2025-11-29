b6aa2af807bcb2f168abfe81138c8d2484bcd5c2
emmacodeya <kkes60128angel@gmail.com>
2025-11-29 13:30:37 +0800

    feat: 更新 README.md，新增網站功能與技術說明；新增彈跳廣告元件樣式與功能；新增產品資料庫

--- src/components/AdPopup.jsx
+++ src/components/AdPopup.jsx
@@ -5,37 +5,48 @@ const AdPopup = () => {
   const [visible, setVisible] = useState(false);
   const { lang } = useContext(LangContext);
 
-
   useEffect(() => {
-    const timer = setTimeout(() => setVisible(true), 300); // 延遲1.5秒出現
+    const timer = setTimeout(() => setVisible(true), 300);
     return () => clearTimeout(timer);
   }, []);
 
   if (!visible) return null;
 
+  const closePopup = () => setVisible(false);
+
   return (
-    <div className="ad-popup-overlay">
-      <div className="ad-popup">
-        <button className="close-btn" onClick={() => setVisible(false)}>×</button>
+    <div className="ad-popup-overlay" onClick={closePopup}>
+      
+      {/* 彈窗本體 */}
+      <div
+        className="ad-popup"
+        onClick={(e) => e.stopPropagation()} // 避免點到裡面也關閉
+      >
+        {/* X 按鈕 */}
+        <button className="close-btn" onClick={closePopup}>
+          ×
+        </button>
+
         <img
           src="/YiHsin/images/discount.jpg"
           alt="限時優惠"
           className="img-fluid rounded my-5"
         />
-        
-         <a
-              href="https://line.me/R/ti/p/@477fjgkd"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn btn-outline-primary-100 rounded-pill px-lg-4 py-lg-2 px-2 py-1 fw-bold"
-            >
-              {lang === "zh-TW"
-                ? "馬上聯繫"
-                : lang === "zh-CN"
-                ? "马上联系"
-                : "Contact Us Now"}
-            </a>
+
+        <a
+          href="https://line.me/R/ti/p/@477fjgkd"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-outline-primary-100 rounded-pill px-lg-4 py-lg-2 px-2 py-1 fw-bold"
+        >
+          {lang === "zh-TW"
+            ? "馬上聯繫"
+            : lang === "zh-CN"
+            ? "马上联系"
+            : "Contact Us Now"}
+        </a>
       </div>
+
     </div>
   );
 };
